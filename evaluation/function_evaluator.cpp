64674b5ee2d6adc17663010c6b4292ead62a5670
ciorant <ant.antoszek@gmail.com>
2025-03-16 21:17:30 +0100

    Implement evaluation of the function on a given domain (#8)

--- evaluation/function_evaluator.cpp
+++ evaluation/function_evaluator.cpp
@@ -1,1 +1,21 @@
 #include "function_evaluator.h"
+#include <limits>
+#include <cmath>
+PlotData* FunctionEvaluator::evaluate(const ParsedFunction& function, double xMin, double xMax, unsigned pointsCount){
+    if (xMin >= xMax) return nullptr;
+    Point* points = new Point[pointsCount];
+    double step = (xMax - xMin) / pointsCount;
+    double yMax = std::numeric_limits<double>::infinity();
+    double yMin = -std::numeric_limits<double>::infinity();
+    unsigned tail = 0;
+    for (int i = 0; i < pointsCount; ++i) {
+      double x = xMin + i*step;
+      double y = function(x);
+      if (isfinite(y)) {
+          if (yMin > y) yMin = y;
+          if (yMax < y) yMax = y;
+        points[tail++] = Point(x, y);}
+    }
+    PlotData* plotData = new PlotData(Rectangle(xMax - xMin, yMax - yMin, Point(xMin, yMin)), points, pointsCount);
+    return plotData;
+  }
