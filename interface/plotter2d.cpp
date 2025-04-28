1c2eca76f4c8faac5873b8600e4d12ccff548f33
Jan Pulkowski <jan.pulkowski@student.uj.edu.pl>
2025-04-28 09:45:55 +0000

    Add axes, grid, and graph colouring  (#21)

--- interface/plotter2d.cpp
+++ interface/plotter2d.cpp
@@ -15,7 +15,7 @@ class FunctionFromLambda final : public ParsedFunction {
 };
 
 void plotter2d::plot(const std::function<double(double)>& func,
-                     const std::pair<double, double>& domain, const Options options) {
+                     const std::pair<double, double>& domain, const Options& options) {
     const FunctionFromLambda function(func);
     Visualizer visualizer(&function, domain.first, domain.second, options);
     visualizer.render();
@@ -23,10 +23,80 @@ void plotter2d::plot(const std::function<double(double)>& func,
 
 void plotter2d::plotFromPolishNotation(const std::string& polishNotation,
                                        const std::pair<double, double>& domain,
-                                       const Options options) {
+                                       const Options& options) {
     FunctionParser parser;
     const ParsedFunction* parsedFunction = parser.parsePolishNotation(polishNotation);
     Visualizer visualizer(parsedFunction, domain.first, domain.second, options);
     visualizer.render();
     delete parsedFunction;
+}
+
+plotter2d::Options::Options(): drawUi(true), drawAxes(true), drawGrid(true),
+                               approximationMode(Points), resolution(5000),
+                               useCustomPlotRange(false), plotRange({}), graphColor(0x000000FF) { }
+
+plotter2d::Options::Options(const bool drawUi, const bool drawAxes, const bool drawGrid,
+                            const ApproximationMode approximationMode, const unsigned resolution,
+                            const bool useCustomPlotRange,
+                            const std::pair<double, double>& plotRange,
+                            const unsigned graphColor) : drawUi(drawUi), drawAxes(drawAxes),
+                                                         drawGrid(drawGrid),
+                                                         approximationMode(approximationMode),
+                                                         resolution(resolution),
+                                                         plotRange(plotRange),
+                                                         useCustomPlotRange(useCustomPlotRange),
+                                                         graphColor(graphColor) { }
+
+plotter2d::OptionsBuilder& plotter2d::OptionsBuilder::drawUi(const bool value) {
+    drawUi_ = value;
+    return *this;
+}
+
+plotter2d::OptionsBuilder& plotter2d::OptionsBuilder::approximationMode(
+    const Options::ApproximationMode mode) {
+    approximationMode_ = mode;
+    return *this;
+}
+
+plotter2d::OptionsBuilder& plotter2d::OptionsBuilder::resolution(const unsigned value) {
+    resolution_ = value;
+    return *this;
+}
+
+plotter2d::OptionsBuilder& plotter2d::OptionsBuilder::drawAxes(const bool value) {
+    drawAxes_ = value;
+    return *this;
+}
+
+plotter2d::OptionsBuilder& plotter2d::OptionsBuilder::graphColor(const unsigned value) {
+    graphColor_ = value;
+    return *this;
+}
+
+plotter2d::OptionsBuilder& plotter2d::OptionsBuilder::plotRange(
+    const std::pair<double, double>& range) {
+    plotRange_ = range;
+    useCustomPlotRange_ = true;
+    return *this;
+}
+
+plotter2d::OptionsBuilder& plotter2d::OptionsBuilder::drawGrid(bool value) {
+    drawGrid_ = value;
+    return *this;
+}
+
+plotter2d::OptionsBuilder& plotter2d::OptionsBuilder::useCustomPlotRange(bool value) {
+    useCustomPlotRange_ = value;
+    if (!value) {
+        plotRange_ = {};
+    }
+    return *this;
+}
+
+plotter2d::Options plotter2d::OptionsBuilder::build() const {
+    bool customPlotRange = useCustomPlotRange_;
+    if (useCustomPlotRange_ && plotRange_ == std::pair<double, double>()) {
+        customPlotRange = false;
+    }
+    return {drawUi_, drawAxes_, drawGrid_, approximationMode_, resolution_, customPlotRange, plotRange_, graphColor_};
 }
--- interface/plotter2d.h
+++ interface/plotter2d.h
@@ -12,33 +12,58 @@ namespace plotter2d {
         };
 
         bool drawUi;
+        bool drawAxes;
+        bool drawGrid;
         ApproximationMode approximationMode;
         unsigned resolution;
         std::pair<double,double> plotRange;
         bool useCustomPlotRange;
+        unsigned graphColor;
 
-        Options() : drawUi(true), approximationMode(Points), resolution(5000),
-                    plotRange(std::make_pair(-INFINITY, INFINITY)), useCustomPlotRange(false) { }
+        Options();
 
-
-        Options(const bool drawUi, const ApproximationMode approximationMode,
-                const unsigned resolution)
-            : drawUi(drawUi), approximationMode(approximationMode), resolution(resolution),
-        plotRange(std::make_pair(-INFINITY, INFINITY)), useCustomPlotRange(false) { }
-
-        Options(const bool drawUi, const ApproximationMode approximationMode,
-        const unsigned resolution, const std::pair<double, double>& plotRange):
-        drawUi(drawUi), approximationMode(approximationMode), resolution(resolution),
-      plotRange(plotRange), useCustomPlotRange(true) { }
+        Options(bool drawUi, bool drawAxes, bool drawGrid, ApproximationMode approximationMode,
+                unsigned resolution, bool useCustomPlotRange, const std::pair<double,double>& plotRange, unsigned graphColor);
 
     };
 
+    class OptionsBuilder {
+        bool drawUi_ = true;
+        Options::ApproximationMode approximationMode_ = Options::Points;
+        unsigned resolution_ = 5000;
+        bool drawAxes_ = true;
+        bool drawGrid_ = true;
+        unsigned graphColor_ = 0x000000FF;
+        std::pair<double,double> plotRange_ {};
+        bool useCustomPlotRange_ = false;
+
+        public:
+            OptionsBuilder& drawUi(bool value);
+
+            OptionsBuilder& approximationMode(Options::ApproximationMode mode);
+
+            OptionsBuilder& resolution(unsigned value);
+
+            OptionsBuilder& drawAxes(bool value);
+
+            OptionsBuilder& drawGrid(bool value);
+
+            OptionsBuilder& graphColor(unsigned value);
+
+            OptionsBuilder& plotRange(const std::pair<double,double>& range);
+
+            OptionsBuilder& useCustomPlotRange(bool value);
+
+            Options build() const;
+    };
+
+
     void plot(const std::function<double(double)>& func, const std::pair<double, double>& domain,
-              Options options = Options());
+              const Options& options = Options());
 
     void plotFromPolishNotation(const std::string& polishNotation,
                                 const std::pair<double, double>& domain,
-                                Options options = Options());
+                                const Options& options = Options());
 }
 
 #endif //PLOTTER2D_H
